114926a693f3977bf59d70cac3c1d80703408993
Hyunchul Ma <hcm0303@gmail.com>
2020-08-05 18:37:52 +0900

    add eureka-server

--- api-gateway/src/main/java/com/dkitec/gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/dkitec/gateway/ApiGatewayApplication.java
@@ -2,9 +2,12 @@ package com.dkitec.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableZuulProxy
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ApiGatewayApplication {
 
