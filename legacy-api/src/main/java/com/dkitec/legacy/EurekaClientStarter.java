b6fb95b9d4c3a000faea4a7de6204a3e86df840b
Hyunchul Ma <hcm0303@gmail.com>
2020-08-07 15:37:14 +0900

    cleaning directory

--- legacy-api/src/main/java/com/dkitec/legacy/EurekaClientStarter.java
+++ legacy-api/src/main/java/com/dkitec/legacy/EurekaClientStarter.java
@@ -1,4 +1,4 @@
-package com.dkitec.legacy.common;
+package com.dkitec.legacy;
 
 import com.netflix.appinfo.ApplicationInfoManager;
 import com.netflix.appinfo.DataCenterInfo;
@@ -67,32 +67,5 @@ public class EurekaClientStarter {
                 .setLeaseInfo(LeaseInfo.Builder.newBuilder().setDurationInSecs(10).build())
                 .build();
     }
-
-    /*@PostConstruct
-    public void postConstruct() {
-        //EurekaClient eurekaClient = EurekaConfiguration.getEurekaClient();
-
-        DiscoveryManager.getInstance().initComponent(new MyDataCenterInstanceConfig(), new DefaultEurekaClientConfig());
-
-        String vipAddress = "MY-SERVICE";
-
-        InstanceInfo nextServerInfo = null;
-        try {
-            nextServerInfo = DiscoveryManager.getInstance()
-                    .getEurekaClient()
-                    .getNextServerFromEureka(vipAddress, false);
-        } catch (Exception e) {
-            System.err.println("Cannot get an instance of example service to talk to from eureka");
-            System.exit(-1);
-        }
-
-        System.out.println("Found an instance of example service to talk to from eureka: "
-                + nextServerInfo.getVIPAddress() + ":" + nextServerInfo.getPort());
-
-        System.out.println("healthCheckUrl: " + nextServerInfo.getHealthCheckUrl());
-        System.out.println("override: " + nextServerInfo.getOverriddenStatus());
-
-        System.out.println("Server Host Name "+ nextServerInfo.getHostName() + " at port " + nextServerInfo.getPort() );
-    }*/
 
 }
--- legacy-api/src/main/java/com/dkitec/legacy/TestController.java
+++ legacy-api/src/main/java/com/dkitec/legacy/TestController.java
@@ -1,4 +1,4 @@
-package com.dkitec.legacy.test;
+package com.dkitec.legacy;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
